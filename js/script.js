febd6b59838f1da4c39cccd5d4e3873832e583ca
mourad-edine <chamsedinemaulice08@gmail.com>
2024-01-10 15:40:41 +0300

    changement au niveau du js

--- js/script.js
+++ js/script.js
@@ -1,57 +1,50 @@
-var message_valeur = document.querySelector(".information").children[1];
-var Prenom, Nom, Matricule, email, dateNaissance, Genre, Numero, Password;
-var valeur;
-//CECI NOUS PERMET DE SELECTIONNER LE 2 EME PARAGRAHPE DANS LA DIV AYANT LA CLASS INFORMATION
-window.onload = () => {
-  valeur = "Aucune valeur";
-  message_valeur.innerHTML = valeur;
-};
-document.forms[0].onchange = () => {
-  console.log("change");
-};
-var qr = new QRious({
-  element: document.querySelector(".qrious"),
-  size: 400,
-  value: valeur,
-});
-function change(element) {
-  switch (element.name) {
-    case "Matricule":
-      Matricule = element.value;
-      break;
+document.addEventListener('DOMContentLoaded', () => {
+  const messageValeur = document.querySelector('.information').children[1];
+  let nom = '', prenom = '', adresse = '', valeur = 'Aucune valeur';
+
+  const qr = new QRious({
+    element: document.querySelector('.qrious'),
+    size: 400,
+    value: valeur,
+  });
+
+  function updateQRCode() {
+    valeur = `- Nom : ${nom} - Prenom : ${prenom} - Adresse : ${adresse}`;
+    qr.value = valeur;
+    messageValeur.innerHTML = qr.value;
   }
 
-  valeur =
-    "- Matricule : " +
-    Matricule
-  qr.value = valeur;
-  message_valeur.innerHTML = qr.value;
-}
+  function handleChange(element) {
+    switch (element.name) {
+      case 'Nom':
+        nom = element.value;
+        break;
+      case 'Prenom':
+        prenom = element.value;
+        break;
+      case 'Adresse':
+        adresse = element.value;
+        break;
+    }
+    updateQRCode();
+  }
 
-// ... Ton code JavaScript existant ...
+  document.forms[0].addEventListener('input', (event) => {
+    if (event.target.tagName === 'INPUT') {
+      handleChange(event.target);
+    }
+  });
 
-// Sélection du bouton de téléchargement
-const downloadButton = document.querySelector(".te");
+  const downloadButton = document.querySelector('.te');
+  downloadButton.addEventListener('click', () => {
+    const qrCodeImage = document.querySelector('.qrious');
+    const downloadLink = document.createElement('a');
+    const qrCodeImageUrl = qrCodeImage.src;
 
-// Ajout d'un gestionnaire d'événements sur le bouton de téléchargement
-downloadButton.addEventListener("click", () => {
-  // Sélection du QR code image
-  const qrCodeImage = document.querySelector(".qrious");
-
-  // Création d'un élément a pour le téléchargement
-  const downloadLink = document.createElement("a");
-
-  // Récupération de l'URL de l'image QR code
-  const qrCodeImageUrl = qrCodeImage.src;
-
-  // Définition des attributs pour le téléchargement
-  downloadLink.href = qrCodeImageUrl;
-  downloadLink.download = "qrcode.png"; // Nom du fichier à télécharger
-
-  // Ajout du lien de téléchargement à la page et déclenchement du téléchargement
-  document.body.appendChild(downloadLink);
-  downloadLink.click();
-
-  // Suppression du lien de téléchargement de la page
-  document.body.removeChild(downloadLink);
+    downloadLink.href = qrCodeImageUrl;
+    downloadLink.download = 'qrcode.png';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  });
 });
